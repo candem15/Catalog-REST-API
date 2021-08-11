5dd7c6ae32f1fbf742d82eff6918aac70229f5b8
Eray <eraybrbr@gmail.com>
2021-08-11 04:42:41 +0300

    Add "Health Check" to REST api
    First thing to do is run this command in terminal specificly for MongoDb
    HealthChecks "dotnet add package AspNetCore.HealthChecks.MongoDb". Then
    go Startup.cs and add HealthChecks services. Atfer that, add endpoints
    for HealthChecks services.

--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,10 @@ using catalog.Settings;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
 
 namespace catalog
 {
@@ -34,11 +38,11 @@ namespace catalog
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); //Guid type representation with not familiar form in MongoDb so we use "serializer" to cover that.
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String)); //DateTimeOffset type representation with not familiar form in MongoDb so we use "serializer" to cover that.
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>(); //For settings of client recieved from "Settings/MongoDbSettings.cs" and use "Configuration" of "Startup" that haven't populated yet. For using mongoDbSettings in other options so we extracted from AddSingleton service.
 
             services.AddSingleton<IMongoClient>(ServiceProvider=> //After setting ConnectionString MongoDb we need to register IMongoClient that we injected into MongoDBItemsRepository. Injecting dependency besides like InMemItemsRepository we have to construct explicitly because of additional configuration needed.
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>(); //For settings of client recieved from "Settings/MongoDbSettings.cs" and use "Configuration" of "Startup" that haven't populated yet.
-                return new MongoClient(settings.ConnectionString); //IMongoClient instance construct.
+                return new MongoClient(mongoDbSettings.ConnectionString); //IMongoClient instance construct.
             });
            
             services.AddSingleton<IItemsRepository,MongoDbItemsRepository>(); //If we need one instance entire lifetime and use it over and over again whenever it is needed, we are register service(dependency) as "Singleton" type to do that.
@@ -51,6 +55,12 @@ namespace catalog
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "catalog", Version = "v1" });
             });
+            services.AddHealthChecks()
+                .AddMongoDb(
+                    mongoDbSettings.ConnectionString, 
+                    name: "mongodb", 
+                    timeout: TimeSpan.FromSeconds(3),
+                    tags: new[]{"ready"}); //That adds HealthCheck service. Now we need to add middleware for it. Added healthcheck option for mongodb with name given "mongo", 3 second timeout and tags "ready".
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +82,30 @@ namespace catalog
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/health/ready",new HealthCheckOptions{  //This pick route that we use to health check endpoint. This works fine but cannot know dependency like MongoDb is healthy or not. Because of that we add nuget package for specificly MongoDB health check =>"dotnet add package AspNetCore.HealthChecks.MongoDb". 
+                    Predicate = (check) => check.Tags.Contains("ready"), //Predicate is the way to filter which health checks we wanna include in this endpoint.
+                    ResponseWriter = async(context, report) => //This used for get more specified response at healthchecks not just like "Healthy or Unhealthy".
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new{
+                                status = report.Status.ToString(), //Healthy or Unhealthy
+                                checks = report.Entries.Select(entry => new {
+                                    name = entry.Key, //Dependency name. In this case "mongodb".
+                                    status = entry.Value.Status.ToString(), //Healthy or Unhealthy again but for this dependency. 
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none", //Exception message if there is any.
+                                    duration = entry.Value.Duration.ToString() //"Health check" response time.
+                                })
+                            }
+                        );
+                        context.Response.ContentType = MediaTypeNames.Application.Json; //This helps to return response nice JSON string format in Postman.
+                        await context.Response.WriteAsync(result);
+                    }
+                }); //This pick route that we use to health check endpoint. This works fine but cannot know dependency like MongoDb is healthy or not. Because of that we add nuget package for specificly MongoDB health check =>"dotnet add package AspNetCore.HealthChecks.MongoDb". 
+                
+                endpoints.MapHealthChecks("/health/live",new HealthCheckOptions{
+                    Predicate = (_) => false //In this case we saying false. By doing that, we are excluding every single health check and so that it would come back to as is service alive.
+                });
             });
         }
     }
